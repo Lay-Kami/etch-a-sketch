48d288243ce2e47c024530d56e0a1e9bcf02b9df
lay <laylafkamilos@gmail.com>
2022-01-02 19:31:59 -0300

    etch-a-sketch.js added page load

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,21 +1,19 @@
 //get user number Input
-const btnGrid = document.querySelector('button#grid-size');
 
 function getUserGrid() {
   let userChoice;
   do {
     userChoice = parseInt(prompt('type between 1 and 100: ', ''));
- } while (userChoice < 1 || userChoice > 100);
- return userChoice;
+  } while (userChoice < 1 || userChoice > 100);
+  return userChoice;
 }
-//dont return, only objects
 
 //make grid and return inBlock squares nodeList
 function makeGrid(number = getUserGrid()) {
   const outBlock = document.querySelector('div.out-block');
   const inBlock = document.querySelector('div.in-block');
   const sketchBoard = document.querySelector('div.sketch-board');
-
+  
   let outBlockClone;
   for (let i = 0; i < number - 1; i++) {
     outBlockClone = outBlock.cloneNode(true);
@@ -33,13 +31,10 @@ function makeGrid(number = getUserGrid()) {
       eachBlock.insertAdjacentElement("beforeend", inBlockClone);
     }  
   });
-
+  
   const inBlockGroup = document.querySelectorAll('.in-block');
   return inBlockGroup;
 }
-
-//invoke
-const blocks = makeGrid(); //nodelist of 36 elements
 
 //set drawing event
 //main hover event
@@ -64,14 +59,6 @@ function resumeDraw(e) {
   });
 }
 
-//only block event
-blocks.forEach((block) => {
-  block.addEventListener('mouseover', makeDraw);
-  block.addEventListener('click', stopDraw);
-  block.addEventListener('dblclick', resumeDraw);
-}); 
-
-//set reset btn
 function resetDraw() {
   const blocks = document.querySelectorAll('.in-block');
   let blocksLength = blocks.length;
@@ -84,16 +71,7 @@ const btnReset = document.querySelector('button');
 btnReset.addEventListener('click', resetDraw); 
 
 //set new grid from user
-btnGrid.addEventListener('click', () => {
-  removeGrid();
-  makeGrid().forEach((block) => {
-    block.addEventListener('mouseover', makeDraw);
-    block.addEventListener('click', stopDraw);
-    block.addEventListener('dblclick', resumeDraw);
-  });
-  const btnReset = document.querySelector('button');
-  btnReset.addEventListener('click', resetDraw); 
-});
+const btnGrid = document.querySelector('button#grid-size');
 
 function removeGrid() {
   const blocks = document.querySelectorAll('div.clone');
@@ -102,4 +80,19 @@ function removeGrid() {
   });
 }
 
-//grab consts and use inside function
+function startDraw() {
+  removeGrid();
+  makeGrid().forEach((block) => {
+    block.addEventListener('mouseover', makeDraw);
+    block.addEventListener('click', stopDraw);
+    block.addEventListener('dblclick', resumeDraw);
+  });
+  const btnReset = document.querySelector('button');
+  btnReset.addEventListener('click', resetDraw); 
+}
+
+startDraw();
+btnGrid.addEventListener('click', startDraw);
+
+
+
